24e1a438d37fd57a7aae1868834bc496ffc29cd1
Azuras03 <nr03russo@gmail.com>
2023-01-31 11:32:14 +0100

    Mise en place d'une page d'incrémentation de variable :]

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,3 +1,45 @@
-<script>
-    
-</script>
+let count = 0;
+let countEl = document.getElementById("count");
+let button = document.getElementById("animateButton");
+let txtSize = 20;
+countEl.style["font-size"] = txtSize + "px";
+
+button.addEventListener("click", function() {
+    countEl.classList.toggle("animate");
+    countEl.innerHTML = "Helo dud";
+});
+
+function decrement() {
+    chgTxtSize(-1);
+    count--;
+    countEl.innerHTML = count;
+}
+
+function increment() {
+    chgTxtSize(1);
+    count++;
+    countEl.innerHTML = count;
+}
+
+function chgTxtSize(size) {
+    if(txtSize == 100){
+        toggleAnimation("rotate");
+    }
+    if(txtSize + size < 5) return;
+    if(txtSize + size > 100) return;
+    if(size < 0) {
+        toggleAnimation("scaleDown");
+    }
+    else {
+        toggleAnimation("scaleUp");
+    }
+    txtSize += size;
+    countEl.style["font-size"] = txtSize + "px";
+}
+
+function toggleAnimation(name) {
+    countEl.classList.toggle(name);
+        setTimeout(function() {
+            countEl.classList.toggle(name);
+        }, 300);
+}
